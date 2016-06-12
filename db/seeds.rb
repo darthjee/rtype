e1998f2b0b1f57ec1dc5515c02a7096dcc15721f
darthjee <darthjee@gmail.com>
2016-06-12 02:37:00 -0300

    Use the current bulk creator

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,2 @@
-Seed::Loader.new(Seed::Race, Definition::Race).create
-Seed::Loader.new(Seed::UnitClass, Definition::UnitClass).create
-Seed::Loader.new(Seed::Unit, Definition::Unit).create
+Seed::Creator.new(:race, :unit_class, :unit).create
 
