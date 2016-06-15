c72291cf2f91f65e8d6d2c1d75c55f604092c5b9
darthjee <darthjee@gmail.com>
2016-06-14 22:37:33 -0300

    Add attackers and defenders spec

--- spec/models/game/battle/event_spec.rb
+++ spec/models/game/battle/event_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe Game::Battle::Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Game::Battle::Event do
+  let(:subject) { game_battle_events(:first_blood) }
+  let(:attacker) { game_battle_participants(:first_attacker) }
+  let(:defender) { game_battle_participants(:first_defender) }
+
+  describe '#attackers' do
+    context 'when event has one attacker and one defender' do
+      it 'returns only the attackers' do
+        expect(subject.attackers).to eq([ attacker ])
+      end
+    end
+  end
+
+  describe '#defenders' do
+    context 'when event has one attacker and one defender' do
+      it 'returns only the defenders' do
+        expect(subject.defenders).to eq([ defender ])
+      end
+    end
+  end
 end
+
