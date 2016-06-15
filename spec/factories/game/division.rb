f239982fb0e47cbcc7416fa2e992499ea25e0c31
darthjee <darthjee@gmail.com>
2016-06-15 19:52:46 -0300

    Add factories

---
+++ spec/factories/game/division.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :division, class: Game::Division do
+    plannet
+  end
+end
+
---
+++ spec/factories/game/squadron.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :squadron, class: Game::Squadron do
+    division
+    unit_id 1
+  end
+end
+
+
